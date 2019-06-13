feeb407716f5becb8c8a468290e982ed4d584d70
singcl <24661881@qq.com>
2019-06-14 01:33:28 +0800

    :art:update

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 // 应用目录是当前目录
 define('APP_PATH', __DIR__ . '/');
 
-// // 开启调试模式
+// 开启调试模式
 define('APP_DEBUG', true);
 
-// // 加载框架文件
+// 加载框架文件
 require(APP_PATH . 'fastphp/Fastphp.php');
 
 // // 加载配置文件
