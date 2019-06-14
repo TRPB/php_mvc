02d133269eea464983b8f11520efb3c1bb80f3b5
singcl <24661881@qq.com>
2019-06-15 02:06:30 +0800

    :art:检测敏感字符并删除

--- fastphp/Fastphp.php
+++ fastphp/Fastphp.php
@@ -19,28 +19,52 @@ class Fastphp
 
     // 运行程序
     public function run()
-    { 
+    {
         // callable 在PHP 中的多种使用形式
         // @see https://segmentfault.com/q/1010000002510575
         spl_autoload_register(array($this, 'loadClass'));
         $this->setReporting();
+        $this->removeMagicQuotes();
     }
 
     // 检测开发环境
-    public function setReporting() {
+    public function setReporting()
+    {
         if (APP_DEBUG === true) {
             error_reporting(EALL);
-            ini_set('display_errors','On');
+            // PHP ini_set用来设置php.ini的值，在函数执行的时候生效，脚本结束后，设置失效。
+            // 无需打开php.ini文件，就能修改配置，对于虚拟空间来说，很方便。 
+            ini_set('display_errors', 'On');
             echo phpinfo();
         } else {
             error_reporting(E_ALL);
-            ini_set('display_errors','Off');
+            ini_set('display_errors', 'Off');
             ini_set('log_errors', 'On');
         }
     }
 
+    // 检测敏感字符并删除
+    public function removeMagicQuotes()
+    {
+        // 当 magic_quotes_gpc 打开时，所有的 ‘ (单引号), ” (双引号), (反斜线) and 空字符会自动转为含有反斜线的溢出字符。
+        if (get_magic_quotes_gpc()) {
+            $_GET = isset($_GET) ? $this->stripSlashesDeep($_GET) : '';
+            $_POST = isset($_POST) ? $this->stripSlashesDeep($_POST) : '';
+            $_COOKIE = isset($_COOKIE) ? $this->stripSlashesDeep($_COOKIE) : '';
+            $_SESSION = isset($_SESSION) ? $this->stripSlashesDeep($_SESSION) : '';
+        }
+    }
+
+    // 删除敏感字符
+    public function stripSlashesDeep($value)
+    {
+        $value = is_array($value) ? array_map(array($this, 'stripSlashesDeep'), $value) : stripslashes($value);
+        return $value;
+    }
+
     // 自动加载类
-    public function loadClass($className) {
+    public function loadClass($className)
+    {
         $classMap = $this->classMap();
 
         if (isset($classMap[$className])) {
@@ -61,7 +85,8 @@ class Fastphp
     }
 
     // 内核文件命名空间映射关系
-    protected function classMap() {
+    protected function classMap()
+    {
         return [
             'fastphp\base\Controller' => CORE_PATH . '/base/Controller.php',
             'fastphp\base\Model' => CORE_PATH . '/base/Model.php',
