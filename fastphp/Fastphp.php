47776cdfc72cf07a8a70602a2f20f9d2c32480fa
singcl <24661881@qq.com>
2019-06-15 16:35:50 +0800

    :art: 框架基本代码结构完成

--- fastphp/Fastphp.php
+++ fastphp/Fastphp.php
@@ -33,11 +33,54 @@ class Fastphp
     // 路由处理
     public function route()
     {
-        if (APP_DEBUG === true) {
-            // https://blog.csdn.net/qq_35433716/article/details/82283824
-            echo "<table><tr><th>" . DB_HOST . "</th><th>" . DB_NAME . "</th></tr></table>";
-            echo phpinfo();
+        $controllerName = $this->config["defaultController"];
+        $actionName = $this->config["defaultAction"];
+        $param = array();
+
+        $url = $_SERVER['REQUEST_URI'];
+        // 清除?之后的内容
+        // 返回字符串在另一字符串中第一次出现的位置，如果没有找到字符串则返回 FALSE
+        $positon = strpos($url, '?');
+        $url = $positon === false ? $url : substr($url, 0, $positon);
+        // 删除前后的“/”
+        $url = trim($url, '/');
+        if ($url) {
+            // 使用“/”分割字符串，并保存在数组中
+            $urlArray = explode('/', $url);
+            // 删除空的数组元素
+            $urlArray = array_filter($urlArray);
+            // 获取控制器名
+            $controllerName = ucfirst($urlArray[0]);
+            // 获取动作名
+            array_shift($urlArray);
+            $actionName = $urlArray ? $urlArray[0] : $actionName;
+            // 获取URL参数
+            array_shift($urlArray);
+            $param = $urlArray ? $urlArray : array();
+            // echo "$controllerName $actionName", var_dump($param);
         }
+
+        // 判断控制器和操作是否存在
+        $controller = 'app\\controllers\\' . $controllerName . 'Controller';
+        if (!class_exists($controller)) {
+            exit($controller . '控制器不存在');
+        }
+        if (!method_exists($controller, $actionName)) {
+            exit($actionName . '方法不存在');
+        }
+
+        // // 当没有使用 use some\namespace\Class 的时候可以直接 new  some\\namespace\\Class 的方式使用类
+        // $obj = new $controller();
+        // $obj->index();
+
+        // 如果控制器和操作名存在，则实例化控制器，因为控制器对象里面
+        // 还会用到控制器名和操作名，所以实例化的时候把他们俩的名称也传进去
+        // 结合Controller基类一起看
+        $dispatch = new $controller($controllerName, $actionName);
+
+        // $dispatch保存控制器实例化后的对象，我们就可以调用它的方法，
+        // 也可以像方法中传入参数，以下等同于：$dispatch->$actionName($param)
+        call_user_func_array(array($dispatch, $actionName), $param);
     }
 
     // 检测开发环境
