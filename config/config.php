0d41da9f4ffe8dfb854706fb7126d440ed7b44e1
singcl <24661881@qq.com>
2019-06-16 12:55:01 +0800

    :art: 修改数据库配置

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 // 数据库配置
 $config['db']['host'] = 'localhost';
-$config['db']['username'] = 'php_mvc';
-$config['db']['password'] = 'mvc520';
-$config['db']['dbname'] = 'php_mvc';
+$config['db']['username'] = 'singcl';
+$config['db']['password'] = '';
+$config['db']['dbname'] = 'project';
 
 // 默认控制器和操作名
 $config['defaultController'] = 'Item';
