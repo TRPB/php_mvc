511781f39996f3bbe41f39c66cb163e425c2314c
singcl <24661881@qq.com>
2019-06-16 12:09:50 +0800

    Model基类 Sql 基类

---
+++ fastphp/db/Sql.php
@@ -0,0 +1,86 @@
+<?php
+namespace fastphp\db;
+
+use \PDOStatement;
+
+class Sql
+{
+    // 数据库表名
+    protected $table;
+    // 数据库主键
+    protected $primary = 'id';
+    // WHERE和ORDER拼装后的条件
+    private $filter = '';
+    // Pdo bindParam()绑定的参数集合
+    private $param = array();
+
+    /**
+     * 查询条件拼接，使用方式：
+     *
+     * $this->where(['id = 1','and title="Web"', ...])->fetch();
+     * 为防止注入，建议通过$param方式传入参数：
+     * $this->where(['id = :id'], [':id' => $id])->fetch();
+     *
+     * @param array $where 条件
+     * @return $this 当前对象
+     */
+    public function where($where = array(), $param = array())
+    {
+        if ($where) {
+            $this->filter .= ' WHERE ';
+            $this->filter .= implode(' ', $where);
+            $this->param = $param;
+        }
+        return $this;
+    }
+
+    /**
+     * 拼装排序条件，使用方式：
+     *
+     * $this->order(['id DESC', 'title ASC', ...])->fetch();
+     *
+     * @param array $order 排序条件
+     * @return $this
+     */
+    public function order($order = array())
+    {
+        if ($order) {
+            $this->filter .= ' ORDERBY ';
+            $this->filter .= implode(',', $order);
+        }
+        return $this;
+    }
+
+    /**
+     * 占位符绑定具体的变量值
+     * @param PDOStatement $sth 要绑定的PDOStatement对象
+     * @param array $params 参数，有三种类型：
+     * 1）如果SQL语句用问号?占位符，那么$params应该为
+     *    [$a, $b, $c]
+     * 2）如果SQL语句用冒号:占位符，那么$params应该为
+     *    ['a' => $a, 'b' => $b, 'c' => $c]
+     *    或者
+     *    [':a' => $a, ':b' => $b, ':c' => $c]
+     *
+     * @return PDOStatement
+     */
+    public function formatParam(PDOStatement $sth, $params = array())
+    {
+        foreach ($params as $param => &$value) {
+            $param = is_int($param) ? $param + 1 : ':' . ltrim($param, ':');
+            $sth->bindParam($param, $value);
+        }
+        return $sth;
+    }
+
+    // 查询所有
+    public function fetchAll()
+    {
+        $sql = sprintf("select * from `%s` %s", $this->table, $this->filter);
+        $sth = Db::pdo()->prepare($sql);
+        $sth = $this->formatParam($sth, $this->param);
+        $sth->execute();
+
+        return $sth->fetch();
+    }
+}
