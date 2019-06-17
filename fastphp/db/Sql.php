d85efef443e05d4c514ce12e08ac8f4012d9daf5
singcl <24661881@qq.com>
2019-06-17 19:47:29 +0800

    :bug: %s 寫成了 s% ！！！

--- fastphp/db/Sql.php
+++ fastphp/db/Sql.php
@@ -95,7 +95,7 @@ class Sql
     {
         $fields = array();
         foreach ($data as $key => $value) {
-            $fields[] = sprintf("`s%` = :%s", $key, $key);
+            $fields[] = sprintf("`%s` = :%s", $key, $key);
         }
 
         return implode(',', $fields);
@@ -149,7 +149,7 @@ class Sql
     // 修改数据
     public function update($data)
     {
-        $sql = sprintf("update `%s` set s% s%", $this->table, $this->formatUpdate($data), $this->filter);
+        $sql = sprintf("update `%s` set %s %s", $this->table, $this->formatUpdate($data), $this->filter);
         $sth = Db::pdo()->prepare($sql);
         $sth = $this->formatParam($sth, $data);
         $sth = $this->formatParam($sth, $this->param);
