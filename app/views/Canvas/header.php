789ea5c5d73d1bd8efebac45716ac9342bcc43af
singcl <24661881@qq.com>
2019-06-19 18:06:35 +0800

    :art: Canvas 绘制形状

--- app/views/Canvas/header.php
+++ app/views/Canvas/header.php
@@ -6,4 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $title ?></title>
+    <style>
+        .graph>* {
+            display: inline-block;
+            vertical-align: top;
+            text-align: center;
+        }
+    </style>
 </head>
--- app/views/Canvas/index.php
+++ app/views/Canvas/index.php
@@ -1,7 +1,48 @@
   <h1><?php echo $title ?></h1>
 
   <body onload="draw();">
+      <h3>1.基本用法</h3>
       <canvas id="canvas" width="150" height="150"></canvas>
       <canvas id="canvas2" width="150" height="150"></canvas>
+
+      <h3>2.使用canvas来绘制图形</h3>
+      <div class="graph">
+          <div>
+              <h4>绘制矩形</h4>
+              <canvas id="canvas3" width="150" height="150"></canvas>
+          </div>
+          <div>
+              <h4>绘制路径 - 三角形</h4>
+              <canvas id="canvas4" width="150" height="150"></canvas>
+          </div>
+          <div>
+              <h4>绘制路径 - 移动笔触</h4>
+              <canvas id="canvas5" width="150" height="150"></canvas>
+          </div>
+          <div>
+              <h4>绘制路径 - 线</h4>
+              <canvas id="canvas6" width="150" height="150"></canvas>
+          </div>
+          <div>
+              <h4>绘制路径 - 圆弧</h4>
+              <canvas id="canvas7" width="150" height="150"></canvas>
+          </div>
+          <div>
+              <h4>绘制路径 - 二次贝塞尔曲线</h4>
+              <canvas id="canvas8" width="150" height="150"></canvas>
+          </div>
+          <div>
+              <h4>绘制路径 - 三次贝塞尔曲线</h4>
+              <canvas id="canvas9" width="150" height="150"></canvas>
+          </div>
+          <div>
+              <h4>绘制路径 - 组合使用</h4>
+              <canvas id="canvas10" width="200" height="200"></canvas>
+          </div>
+          <div>
+              <h4>绘制路径 - Path2D对象</h4>
+              <canvas id="canvas11" width="200" height="200"></canvas>
+          </div>
+      </div>
       <script src="/static/js/canvas.js"></script>
   </body>
