25f4e3e5816822221013d3e45376d78baeccd530
? <?@localhost>
2019-11-21 21:03:42 +0800

    :art: 数据库配置 - 发布STUDIO 云配置

--- config/config.php
+++ config/config.php
@@ -5,6 +5,12 @@ $config['db']['username'] = 'singcl';
 $config['db']['password'] = 'singcl';
 $config['db']['dbname'] = 'project';
 
+// // 数据库配置 - 发布STUDIO 云配置
+// $config['db']['host'] = $_ENV['MYSQL_HOST'];
+// $config['db']['username'] = $_ENV['MYSQL_USERNAME'];
+// $config['db']['password'] = $_ENV['MYSQL_PASSWORD'];
+// $config['db']['dbname'] =   $_ENV['MYSQL_DBNAME'];
+
 // 默认控制器和操作名
 $config['defaultController'] = 'Item';
 $config['defaultAction'] = 'index';
